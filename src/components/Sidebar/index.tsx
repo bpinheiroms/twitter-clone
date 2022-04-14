829b4dfbdbf35f7253a0795599bdb30d3f09cd70
Bruno Pinheiro <bpinheiroms@MacBook-Air-de-Bruno.local>
2022-04-13 22:03:10 -0300

    fix: responsible sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -4,7 +4,7 @@ import SidebarLink from '../SideBarLink';
 
 function Sidebar() {
   return (
-    <div className="sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
+    <div className="md:fixed sm:fixed flex-col items-center xl:items-start xl:w-[340px] p-2 h-full">
       <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
         <SidebarLink text="Home" Icon={HomeIcon} />
         <SidebarLink text="User Profile" Icon={UserIcon} />
